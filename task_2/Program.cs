4058a996c9263d1c4a9445d3b198ca80160831d2
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-13 23:31:36 +0300

    add task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int max;
+
+Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+Console.WriteLine($"max = {max}");
