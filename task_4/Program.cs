c9b871908c846ec8cde3dca82b5dabd49d90321d
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-13 23:53:50 +0300

    add task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿int countNumber = 3, 
+    counter = 1;
+int? max = null,
+    number = null;
+
+while (counter <= countNumber)
+{
+    Console.Write("Введите число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    
+    if (max == null) max = number;
+    if (max < number) max = number;
+
+    counter ++;
+}
+
+Console.WriteLine($"Максимальное значение {max}");
