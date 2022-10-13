e1024305950a019d2621bd6c092dbe277371cff8
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-14 00:10:27 +0300

    add task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number > 1)
+{
+    int nowNumber = 2;
+    while (nowNumber <= number)
+    {
+        Console.Write($"{nowNumber} ");
+        nowNumber += 2;
+    }
+}
+else
+{
+    Console.Write("Четных чисел нет");
+}
